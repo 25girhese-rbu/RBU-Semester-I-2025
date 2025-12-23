829e1590444c8d1fc48dfae21473889e6feefd37
Soniya Eknath Girhe <girhesoniya@gmail.com>
2025-12-23 12:58:26 +0530

    Implement linear search in Linear_Search.c

---
+++ 05-Arrays-and-Sorting/Linear_Search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10], n, i, key, found = 0;
+
+    printf("Enter number of elements (max 10): ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the number to search: ");
+    scanf("%d", &key);
+
+    // Linear Search Logic
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            printf("Element found at position %d\n", i + 1);
+            found = 1;
+            break; 
+        }
+    }
+
+    if (!found) {
+        printf("Element not found in the array.\n");
+    }
+
+    return 0;
+}
