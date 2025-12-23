57e16cd9558d24112d45c05a09aaac44a208d393
Soniya Eknath Girhe <girhesoniya@gmail.com>
2025-12-23 12:51:37 +0530

    Add Series Sum calculation with factorial function
    This program calculates the sum of a series using factorials.

---
+++ 04-Functions-and-Recursion/Series_Sum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// User-defined function to calculate factorial
+// 'double' is used to handle large numbers like 10!
+double findFact(int n) {
+    double f = 1;
+    for (int i = 1; i <= n; i++) {
+        f = f * i;
+    }
+    return f;
+}
+
+int main() {
+    int N;
+    float sum = 0;
+
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+
+    // Loop through each term from 1 to N
+    for (int i = 1; i <= N; i++) {
+        // (float) converts 'i' so the division gives a decimal result
+        sum = sum + (float)i / findFact(i);
+    }
+
+    printf("Total sum of the series is: %.4f\n", sum);
+
+    return 0;
+}
